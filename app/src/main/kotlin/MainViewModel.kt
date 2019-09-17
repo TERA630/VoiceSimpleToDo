945623d5e936c8ba5ecf7122406900973a4f0961
TERA630 <tera.bcool.4475@gmail.com>
2019-09-17 21:53:13 +0900

    swipeToDismiss was made card

--- app/src/main/kotlin/MainViewModel.kt
+++ app/src/main/kotlin/MainViewModel.kt
@@ -29,18 +29,10 @@ class MainViewModel : ViewModel() {
         result.add(ItemEntity(2, "天気を確認する", "スマホ", "準備", isParent = true, isChild = false))
         result.add(ItemEntity(3, "服に着替える", "自転車通勤か電車通勤か､研究会があるか", "準備", isParent = true, isChild = false))
         result.add(ItemEntity(4, "口を洗浄する", "うがい､歯磨き", tag = "準備", isParent = true, isChild = false))
-        //   result.add(ItemEntity(5,"洗口液","使えば無くなる",tag = "準備",isParent = false,isChild = true,isClosed = true,isChildOf = 4))
+        result.add(ItemEntity(5,"洗口液","使えば無くなる",tag = "準備",isParent = false,isChild = true,isClosed = true,isChildOf = 4))
         result.add(ItemEntity(6, "髪を整える", "ヘアスプレー", "準備", isParent = true, isChild = false))
-        //   result.add(ItemEntity(7,"櫛を入れる","","準備",false,isClosed = true,isChild = true,isChildOf = 6))
-        result.add(
-            ItemEntity(
-                8,
-                "プロテインを作る",
-                "3杯､可能なら牛乳を入れる",
-                "準備",
-                isParent = true,
-                isChild = false
-            )
+        result.add(ItemEntity(7,"櫛を入れる","","準備",false,isClosed = true,isChild = true,isChildOf = 6))
+        result.add(ItemEntity(8, "プロテインを作る", "3杯､可能なら牛乳を入れる", "準備", isParent = true, isChild = false)
         )
         return result
     }
