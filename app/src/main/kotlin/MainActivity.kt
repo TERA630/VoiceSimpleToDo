fbf8cd2d9709be4725fbed0c76e2dff17a372cc3
TERA630 <tera.bcool.4475@gmail.com>
2019-11-17 23:05:08 +0900

    making audio Permission

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.voicesimpletodo
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -15,7 +20,7 @@ const val PREFS = "SpeechService"
 class  MainActivity : AppCompatActivity() {
 
     private val vModel by viewModel<MainViewModel>()
-
+    private val mRequestCodeRecord = 1
     // Activity Lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,26 @@ class  MainActivity : AppCompatActivity() {
         constructViews(savedInstanceState)
 //        val configuration = Configuration.Builder().setWorkerFactory(MyWorkerFactory(vModel)).build()
 //        WorkManager.initialize(this.applicationContext,configuration)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val audioPermission = ContextCompat.checkSelfPermission(this.baseContext, Manifest.permission.RECORD_AUDIO)
+        when {
+            audioPermission == PackageManager.PERMISSION_GRANTED -> {
+                startVoiceRecorder()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) -> {
+                AlertDialog.Builder(this)
+                    .setTitle("permission")
+                    .setMessage("このアプリの利用には音声の録音を許可してください.")
+                Log.w("test", "permission request was disabled")
+            }
+            else -> {
+                Log.w("test", "this app has no permission yet.")
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECORD_AUDIO), mRequestCodeRecord)
+            }
+        }
     }
     override fun onPause() {
         super.onPause()
