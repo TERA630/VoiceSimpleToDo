d0dc936624fb3ea9280f2625d33d0616b4089aad
TERA630 <tera.bcool.4475@gmail.com>
2019-11-28 15:15:43 +0900

    tuning recognize..

--- app/src/main/kotlin/VoiceRecorder.kt
+++ app/src/main/kotlin/VoiceRecorder.kt
@@ -6,10 +6,10 @@ import android.media.MediaRecorder
 import android.util.Log
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
-import kotlin.experimental.and
+import kotlin.math.abs
 
 const val AUDIO_BUFFER_SIZE = 16 * 100 * 2  // sampling per ms * 100 ms
-const val MAX_SPEECH_LENGTH_MILLIS = 30 * 1000
+const val MAX_SPEECH_LENGTH_MILLIS = 10 * 1000 // Original 30000
 const val SPEECH_TIMEOUT_MILLIS = 2000
 
 // Dependence  MainViewModel, SpeechStreaming (InitずみでResponseServerが建っている｡)
@@ -31,14 +31,14 @@ class VoiceRecorder(val scope:CoroutineScope,val vModel: MainViewModel){
         for(sampleRate in cSampleRateCandidates){
             val sizeInBytes = AudioRecord.getMinBufferSize(sampleRate,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT)
             if(sizeInBytes == AudioRecord.ERROR_BAD_VALUE) continue // このサンプリングレートで動作しない場合は次の候補に移る。
-            val audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT, sizeInBytes)
+            val audioRecord = AudioRecord(MediaRecorder.AudioSource.MIC,sampleRate,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT, AUDIO_BUFFER_SIZE)
             if(audioRecord.state != AudioRecord.STATE_INITIALIZED) {
                 // AudioRecordが取得できない場合　(Ex. permissionがない..)
                 audioRecord.release()
                 isAudioRecordEnabled = false
                 Log.w("AudioRecord","error in instantiating AudioRecord ${audioRecord.state}")
             } else {
-                mBuffer = ByteArray(sizeInBytes)
+                mBuffer = ByteArray(AUDIO_BUFFER_SIZE)
                 mAudioRecord = audioRecord
                 isAudioRecordEnabled = true
                 return sampleRate
@@ -53,15 +53,16 @@ class VoiceRecorder(val scope:CoroutineScope,val vModel: MainViewModel){
             while (isActive) {
                 val size = mAudioRecord.read(mBuffer, 0, mBuffer.size) // size は　AudioRecordで得られたデータ数
                 val now = System.currentTimeMillis()
+                Log.i(mTag,"$size was read at $now ")
                 if (isHearingVoice(mBuffer, size)) {
-                    loudVoiceProcess(size,looptime = now)
+                    loudVoiceProcess(size,loopTime = now)
                 } else if(mLastVoiceHeardMillis != Long.MAX_VALUE) { // 無音かつ一度有音があった場合
                     vModel.speechStreaming.recognize(mBuffer,size)
                     if(now - mLastVoiceHeardMillis > SPEECH_TIMEOUT_MILLIS){ // 無音期間がタイムアウト以上になった場合
                         finishRecognizing()
                     }
                 }
-                delay(10)
+                delay(70)
             }
             mAudioRecord.stop()
         }
@@ -75,22 +76,29 @@ class VoiceRecorder(val scope:CoroutineScope,val vModel: MainViewModel){
         mLastVoiceHeardMillis = Long.MAX_VALUE
     }
 
-    private fun loudVoiceProcess(size:Int,looptime:Long){
+    private fun loudVoiceProcess(size:Int, loopTime:Long){
         if(mLastVoiceHeardMillis == Long.MAX_VALUE) { // 閾値以上のAudioDataが得られたとき
-            mStartSteamRecognizingMills = looptime
+            mStartSteamRecognizingMills = loopTime
             vModel.speechStreaming.buildRequestServer()
         }
         vModel.speechStreaming.recognize(mBuffer,size)
-        mLastVoiceHeardMillis = looptime
-        if(looptime - mStartSteamRecognizingMills > MAX_SPEECH_LENGTH_MILLIS) {
+        mLastVoiceHeardMillis = loopTime
+        if(loopTime - mStartSteamRecognizingMills > MAX_SPEECH_LENGTH_MILLIS) {
             finishRecognizing()
         }
     }
     private fun isHearingVoice(buffer: ByteArray, size: Int): Boolean {
-        for (i in 0 until size - 1 step 2) { // Android writing out big endian  ex. 0x0c0f →　0x0f0c
-            var upperByte = buffer[i + 1] // Little endian  上位バイト　　　　　　  ex.  s = 00ff 00cc 0048 2001 0005
-            upperByte = upperByte and ( 0x07)                                           // オリジナルの閾値が1500 = 0x05dc
-            if(upperByte>=0x07) return true
+        var i = 0
+        while (i < size - 1) {
+            // The buffer has LINEAR16 in little endian.
+            var s = buffer[i + 1].toInt()
+            if (s < 0) s *= -1
+            s = s shl 8
+            s += abs (buffer[i].toInt())
+            if (s > 1500) {
+                return true
+            }
+            i += 2
         }
         return false
     }
